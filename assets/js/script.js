beb658035de9e6f1fd0f1d184c264ee6ab6a9ac3
RTAKA808 <106231361+RTAKA808@users.noreply.github.com>
2024-04-05 00:01:37 -0700

    input takes in new cities and populating new div

---
+++ assets/js/script.js
@@ -0,0 +1,94 @@
+// form inputs
+//display current and future conditions/saved to search history
+//current weather for selected city
+///city name, date, icon,temperature, humidity, windspeed  ( weather/icon)
+//5 day forcast 
+//clickable history
+
+const searchBox = document.querySelector('#searchBox')
+const today = document.querySelector('#today')
+const fiveDay = document.querySelector('#fiveDay') 
+const cards= document.querySelector('#cards')
+const button=document.querySelector('#button')
+const weatherAppAPIKey = "d3544544038162ae81f0a4e78fb687cd"
+
+
+
+
+button.addEventListener('click',function(event){
+event.preventDefault();
+let city=searchBox.value
+let storedWeather={
+    location:searchBox.value
+}
+let history=JSON.parse(localStorage.getItem('locations'))
+if(!history){
+    history=[]
+}
+history.push(storedWeather)
+
+if(!storedWeather.location){
+    alert('Please enter a location')
+}else{
+localStorage.setItem('locations',JSON.stringify(history))
+searchBox.value=''
+}
+
+
+const url =`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${weatherAppAPIKey}`
+
+fetch(url).then(function(response){
+    return response.json();
+}).then(function(data){
+    console.log("--------- First request with geolocation --------")
+    console.log(data);
+
+    const latitude = data[0].lat;
+    const longitude = data[0].lon;
+    console.log(latitude, longitude);
+
+    const url2 = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${weatherAppAPIKey}`
+    fetch(url2).then(function(response2) {
+        return response2.json();
+    }).then(function(data2){
+        console.log("--------- Second request with forecase --------")
+        console.log(data2);
+
+       
+    })
+
+})
+//insert f2
+})
+
+function mainDisplay(){
+
+
+}
+
+//create task card
+function displays(){
+    let container=document.createElement('div');
+    let cityName=document.createElement('h2')
+    let cardDate=document.createElement('p');
+    let temperature=document.createElement('p');
+    let wind=document.createElement('p');
+    let humidity=document.createElement('p');
+    let icon=document.createElement('p');
+cityName.innerHTML=
+cardDate.innerHTML=
+temperature.innerHTML=
+wind.innerHTML=
+humidity.innerHTML=
+icon.innerHTML=
+
+
+today.appendChild(container)
+container.appendChild(cityName)
+container.appendChild(cardDate)
+container.appendChild(icon)
+container.appendChild(temperature)
+container.appendChild(wind)
+container.appendChild(humidity)
+container.className='newDiv'
+}
